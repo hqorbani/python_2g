1f8c3fc8e04bf636bd6a4e3b70d539171afc7a21
Hamzeh <h.qorbani7@gmail.com>
2023-09-10 22:30:39 +0330

    session_3

--- session_1/sample_2/main.py
+++ session_1/sample_2/main.py
@@ -1,9 +1,10 @@
 from person import person
 
-person_1 = person("Hamzeh" ,"Qorbani" , "09159862132")
-person_2 = person("Fatemeh" ,"Valipour" , "09159860000")
+person_1 = person("Hamzeh" ,"Qorbani" , "09159862132", "green")
+person_2 = person("Fatemeh" ,"Valipour" , "09159860000","green")
 
 # print(person_1.name , person_1.mobile)
 # print(person_2.name , person_2.mobile)
 print(person_1.introduce_yourself())
 print(person_2.introduce_yourself())
+print(person_1.say_hello())
--- session_1/sample_2/person.py
+++ session_1/sample_2/person.py
@@ -1,10 +1,12 @@
 class person:
-    def __init__(self , name , family , mobile) :
-        self.name = name
-        self.family = family
-        self.mobile = mobile
+    #magic methods
+    def __init__(self , name , family , mobile , color) :
+        self.name = name #property
+        self.family = family #property
+        self.mobile = mobile #property
+        self.color = color #property
         
     def introduce_yourself(self):
         return f"I'm {self.name} {self.family} and my mobile is {self.mobile}"
-    def say_hello():
-        return "Hello dear,"
+    def say_hello(self):
+        return "Hello dear," + self.color
