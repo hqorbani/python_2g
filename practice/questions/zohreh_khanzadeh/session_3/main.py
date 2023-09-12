9e1ae4f0a1aa0933bcd116d99167eb76a94e098d
Hamzeh <h.qorbani7@gmail.com>
2023-09-12 22:55:14 +0430

    session_3:practice:zohreh_khanzadeh

---
+++ practice/questions/zohreh_khanzadeh/session_3/main.py
@@ -0,0 +1,8 @@
+from trader import trader
+
+trader_1 = trader("Hamzeh" ,"Qorbani" , "precious metals")
+trader_2 = trader("zohre" ,"khanzade" , "BITCOIN")
+
+print(trader_1.introduce_yourself())
+print(trader_2.introduce_yourself())
+print(trader_1.say_hello())
