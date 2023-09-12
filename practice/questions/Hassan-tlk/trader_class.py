5c8bd309df4fe13931c2d4dd0525436c1ad89150
Hamzeh <h.qorbani7@gmail.com>
2023-09-12 23:03:34 +0430

    session_3: practice answer edited

--- practice/questions/Hassan-tlk/trader_class.py
+++ practice/questions/Hassan-tlk/trader_class.py
@@ -1,9 +1,7 @@
 class Trader:
-    def __init__(self, stop_lose, buy_limit):
+    def __init__(self, stop_lose, take_profit):
         self.stop_lose = stop_lose
-        self.buy_limit = buy_limit
-
-
+        self.take_profit = take_profit
     def buy_arz(self):
         pass
 
