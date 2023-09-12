bc9053bf9829935ad50b0cd210d487b55420e7e2
Hamzeh <h.qorbani7@gmail.com>
2023-09-12 22:37:07 +0430

    session_3: practice answer: mohammad_mahdi_nasiri

---
+++ practice/questions/mohammad_mahdi_nasiri/session_3/trader.py
@@ -0,0 +1,8 @@
+class trader :
+    def __init__(self , profit_limit , stop_limit):
+        self.profit_limit = profit_limit
+        self.stop_limit = stop_limit
+    def buy(self):
+        pass
+    def sell(self):
+        pass
