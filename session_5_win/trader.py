51ac4624b63ef05e41bd9ac3052bf1794dd75f4f
Hamzeh <h.qorbani7@gmail.com>
2023-09-17 19:02:46 +0430

    session_5_win: acocunt_info

---
+++ session_5_win/trader.py
@@ -0,0 +1,27 @@
+import MetaTrader5 as mt5
+import pandas as pd
+class trader:
+    # def __init__(self, username) -> None:
+    #     self.username = username
+    def connect():
+        if mt5.initialize():
+            return True
+        else:
+            return False
+        
+    def get_account_info():
+        return mt5.account_info()._asdict()
+    
+
+
+    def check_buy():
+        return True
+    def check_sell():
+        return True
+    def open_position(p_type):
+        return p_type
+
+        
+
+
+
