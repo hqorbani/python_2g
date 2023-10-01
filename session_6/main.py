0b6dd9534a921d6265f3ba5301dc5fb7cf0c1a55
Hamzeh <h.qorbani7@gmail.com>
2023-10-01 19:04:03 +0330

    session_7: get candles and use pandas

--- session_6/main.py
+++ session_6/main.py
@@ -1,11 +1,18 @@
 from trader import trader
-
+import MetaTrader5 as mt5
 bot_1 = trader
 if bot_1.connect():
     # account_info = bot_1.get_account_info()
     # for account_key , account_value in account_info.items():
     #     print(account_key,"==>" , account_value)
-    pass
+    symbole  = "EURUSD"
+    tm = 1
+    start = 0
+    number = 1500
+    candles = bot_1.get_candles(symbole , tm , start , number)
+    # print(type(candles))
+    print(candles)
+    print(candles.iloc[-2].high)
 
 
 
--- session_6/trader.py
+++ session_6/trader.py
@@ -1,5 +1,5 @@
 import MetaTrader5 as mt5
-# import pandas as pd
+import pandas as pd
 class trader:
     # def __init__(self, username) -> None:
     #     self.username = username
@@ -15,10 +15,18 @@ class trader:
     def get_symbols(str_symbols):
         return mt5.symbols_get(str_symbols)
 
-    def get_candles():
+    def get_candles(symbole , tm , start , number):
+        # symbole , tm , start , number 
         # این متد قراره اطلاعات کندلی مربوط به 15 کندل اخیر را دریافت کند
         # ورودی های متد را خودتان مشخص کنید
-        pass
+        rates = mt5.copy_rates_from_pos(symbole , tm , start , number)
+        rates_frame = pd.DataFrame(rates)
+        rates_frame.drop('real_volume' , inplace = True , axis= 1)
+        rates_frame.drop('tick_volume' , inplace = True , axis= 1)
+        rates_frame['time'] = pd.to_datetime(rates_frame['time'] , unit= 's')
+        # rates_frame['new_open'] = rates_frame['open'] + 2
+        return rates_frame
+
 
     
         
