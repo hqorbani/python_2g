590fd2d4e13156ebad3add4fd27428bf55ff239a
Hamzeh <h.qorbani7@gmail.com>
2023-10-10 18:34:07 +0330

    create schedule for bot

--- session_6/main.py
+++ session_6/main.py
@@ -1,5 +1,7 @@
 from trader import trader
-import MetaTrader5 as mt5
+import schedule
+import time
+
 bot_1 = trader
 if bot_1.connect():
     # account_info = bot_1.get_account_info()
@@ -9,11 +11,9 @@ if bot_1.connect():
     tm = 1
     start = 0
     number = 15
+def run_trader(bot_1):
     candles = bot_1.get_candles(symbole , tm , start , number)
-    # print(type(candles))
-
     rate_frame = bot_1.extend_columns(candles)
-    # print(rate_frame)``
     if bot_1.ready_buy(rate_frame):
         print("buy")
     elif bot_1.ready_sell(rate_frame):
@@ -21,11 +21,8 @@ if bot_1.connect():
     else:
         print("nothing")
 
+schedule.every(3).seconds.do(run_trader , bot_1)
 
-
-
-
-    # if bot_1.check_buy():
-    #      print(bot_1.open_position('buy'))
-    # elif bot_1.check_sell():
-    #     print(bot_1.open_position('sell'))
+while True:
+    schedule.run_pending()
+    time.sleep(1)
